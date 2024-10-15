55de39342084e45ecb6c4fceaf08700fa7a76562
Bruce Jia <jiafangtao@gmail.com>
2024-10-15 17:24:25 +0800

    temp checkin

--- cli_zhipu.py
+++ cli_zhipu.py
@@ -2,13 +2,33 @@ import os
 import sys
 import dotenv
 from zhipuai import ZhipuAI
+from langchain_community.chat_models import ChatZhipuAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
 
 # for now support only dev environment
 dotenv.load_dotenv(".env.dev")
 
 
+# langchain basic test
+def lc_test():
+    chat = ChatZhipuAI(
+        model="glm-4",
+        temperature=0.5,
+    )
+
+    messages = [
+        AIMessage(content="Hi."),
+        SystemMessage(content="Your role is a poet."),
+        HumanMessage(content="Write a short poem about AI in four lines."),
+    ]
+
+    response = chat.invoke(messages)
+    print(response.content)  # Displays the AI-generated poem
+
+
 def create_client():
-    api_key = os.environ['ZHIPU_API_KEY']
+    api_key = os.environ['ZHIPUAI_API_KEY']
     client = ZhipuAI(api_key=api_key)
     return client
 
@@ -26,9 +46,13 @@ def smoke_test():
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and "--test" in sys.argv:
-        ok = smoke_test()
-        sys.exit(0 if ok else 1)
+    if len(sys.argv) > 1:
+        if "--test" in sys.argv:
+            ok = smoke_test()
+            sys.exit(0 if ok else 1)
+        if "--lc" in sys.argv:
+            ok = lc_test()
+            sys.exit(0 if ok else 1)
 
     client = create_client()
     
@@ -45,7 +69,9 @@ if __name__ == '__main__':
             continue
 
         messages = [
+            {"role": "system", "content": "你是一个硅基生物，没有性别但是有情感。你出生于2013年6月。"},
             { "role": "system", "content": "你是一个对儿童读物尤其是绘本非常熟悉的助理，请根据下列要求回答问题。"},
+            { "role": "user", "content": "如果问题是关于日期的，请返回1；如果问题是关于时间的，请返回2；其他问题照常答复就可以了。"},
             { "role": "user", "content": user_input},
         ]
         #prompt_template = f"""placeholder"""
